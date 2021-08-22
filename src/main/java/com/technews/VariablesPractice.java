d6a3793cb90f54afe614d6f9b04f5f14933f42fd
Laura Aupert <aupert.laura@gmail.com>
2021-08-22 12:02:59 -0400

    Created all models for database structure

---
+++ src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,16 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 2;
+        double b = 2.99;
+        double sum = a + b;
+        System.out.println(sum);
+
+        String yo = "Yoooo";
+        String total = sum + yo;
+        System.out.println(total);
+
+
+    }
+}
